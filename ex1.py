6dd1520d3af0574d1e25bc1ddbe11ed11b582553
Dinusha <dinusha.diss@gmail.com>
2023-05-24 15:41:05 +0100

    Highest and Lowest values

---
+++ ex1.py
@@ -0,0 +1,19 @@
+# Option 1
+def high_and_low(numbers):
+    num_list = [int(num) for num in numbers.split(' ')]
+    return f"{max(num_list)} {min(num_list)}"
+
+
+# option 2
+def high_and_low(numbers):
+    num_list = list(numbers.split(' '))
+    int_lst = []
+    for num in num_list:
+        int_lst.append(int(num))
+    high = max(int_lst)
+    low = min(int_lst)
+    return f'{high} {low}'
+
+
+high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
+

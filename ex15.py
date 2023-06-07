2176ad5f8365b4bd672af783996b5bd0b67a203d
Dinusha <dinusha.diss@gmail.com>
2023-06-07 17:18:35 +0100

    Testing 1-2-3

--- ex15.py
+++ ex15.py
@@ -1,6 +1,5 @@
 # Testing 1-2-3
 # Option 1 
-
 def func(arr):
     arr_length = len(arr)
     new_arr = []
@@ -11,10 +10,10 @@ def func(arr):
 
 
 # Option 2
-# def func(arr):
-#     arr_length = len(arr)
-#     new_arr = [f"{i}: {arr[i-1]}" for i in range(1, arr_length + 1)]
-#     print(new_arr)
+def func(arr):
+    arr_length = len(arr)
+    new_arr = [f"{i}: {arr[i-1]}" for i in range(1, arr_length + 1)]
+    print(new_arr)
 
 
 

fe28b5548f04099aa45bad8f2911a32c51075207
Dinusha <dinusha.diss@gmail.com>
2023-06-09 15:19:40 +0100

    Counting Duplicates

--- ex17.py
+++ ex17.py
@@ -6,5 +6,9 @@ def func(string):
     duplicates = list(set([item for item in string if string.count(item) > 1]))
     return len(duplicates)
 
+# Option 2
+def func(string):
+    return len([item for item in set(string.lower()) if string.lower().count(item) > 1])
+
 
 func("aA11")

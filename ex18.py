14797273ead12100a6df9acaa2aab4adfbf721ab
Dinusha <dinusha.diss@gmail.com>
2023-06-12 15:28:20 +0100

    Simple Pig Latin

--- ex18.py
+++ ex18.py
@@ -20,4 +20,20 @@ def pig_it(text):
     return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
 
 
+
+# Option 2
+def pig_it(text):
+    lst = text.split()
+    return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
+
+
+
+# Option 3
+def pig_it(text):
+    return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
+
+
+
+
 pig_it('Pig latin is cool !')
+
